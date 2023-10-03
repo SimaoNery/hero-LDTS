80a9aaf1efe0c4f081307472dea0cfd6c050374c
SimaoNery <simaonery@gmail.com>
2023-10-03 11:42:56 +0100

    Added class Game and application now runs game

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,32 +1,9 @@
-import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.screen.TerminalScreen;
-import com.googlecode.lanterna.terminal.Terminal;
-import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import com.googlecode.lanterna.TextCharacter;
-
 import java.io.IOException;
 
 public class Application {
-    public static void main(String[] args) {
-        try {
-            TerminalSize terminalSize = new TerminalSize(40, 20);
-            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+    public static void main(String[] args) throws IOException{
+        Game game = new  Game();
+        game.run();
 
-
-            Terminal terminal = terminalFactory.createTerminal();
-            Screen screen = new TerminalScreen(terminal);
-
-            screen.setCursorPosition(null);
-            screen.startScreen();
-            screen.doResizeIfNecessary();
-
-            screen.clear();
-            screen.setCharacter(10, 10, new TextCharacter('X'));
-            screen.refresh();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
