51549d7687b23bf969c41fc1000f36c51467253a
SimaoNery <simaonery@gmail.com>
2023-10-03 12:29:00 +0100

    Implemented movement and closing screen in Game

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,7 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -9,6 +11,8 @@ import java.io.IOException;
 
 public class Game {
     private Screen screen;
+    private int x = 10;
+    private int y = 10;
     public Game() {
         try {
             TerminalSize terminalSize = new TerminalSize(40, 20);
@@ -25,14 +29,48 @@ public class Game {
             e.printStackTrace();
         }
     }
-
     private void draw() throws IOException{
         screen.clear();
-        screen.setCharacter(10, 10, new TextCharacter('X'));
+        screen.setCharacter(x, y, new TextCharacter('X'));
         screen.refresh();
     }
     public void run() throws IOException{
-        draw();
+       while(true) {
+           draw();
+           KeyStroke key = screen.readInput();
+           if(key.getKeyType() == KeyType.EOF){
+               break;
+           }
+           processKey(key);
+       }
     }
+    private void processKey(KeyStroke key){
+        System.out.println(key);
+        switch(key.getKeyType()) {
+            case ArrowUp:
+                y--;
+                break;
+            case ArrowDown:
+                y++;
+                break;
+            case ArrowLeft:
+                x--;
+                break;
+            case ArrowRight:
+                x++;
+                break;
 
+            case Character:
+                if (key.getCharacter() == 'q') {
+                    try {
+                        screen.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
 }
